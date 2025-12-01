c6b941dd96e2ab0bfbe5bce58ba4246a91956d5f
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-12-01 16:47:56 -0300

    feat(login): inciando estilização da pag de login

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../elements/header/header.component";
 import { FooterComponent } from "../../elements/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public checagemRevendedor = false;
 
 }
