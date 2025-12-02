3e187201ebbc395a1dba94b547b0bf46da9fd66a
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-12-02 16:23:41 -0300

    style(login): melhorando pág de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/elements/header/header.component";
 import { HomeComponent } from "./components/pages/home/home.component";
 import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from "./components/pages/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [ LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
