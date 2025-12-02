2920e0903c1778d7df93d2aeabe1d182f66855cb
Luan <lsantana.duarte7@gmail.com>
2025-12-02 19:18:18 -0300

    feat(app): iniciando as rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/elements/header/header.component";
-import { HomeComponent } from "./components/pages/home/home.component";
+import { HeaderComponent } from './components/elements/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
-import { SignupComponent } from "./components/pages/signup/signup.component";
+import { SignupComponent } from './components/pages/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SignupComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'projeto-final';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "login", component: LoginComponent
+  },
+  {
+    path: "signup", component: SignupComponent
+  }
+];
