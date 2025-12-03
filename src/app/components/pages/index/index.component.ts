49612a16f13cfbc8ab820aaecda512702c9eafec
Luan <lsantana.duarte7@gmail.com>
2025-12-03 19:57:33 -0300

    feat(index): implementando scroll nos carrosséis

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { HeaderAutenticadoComponent } from "../../elements/header-autenticado/header-autenticado.component";
-import { SidebarNavComponent } from '../../elements/sidebar-nav/sidebar-nav.component';
+  import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+  import { HeaderAutenticadoComponent } from '../../elements/header-autenticado/header-autenticado.component';
+  import { SidebarNavComponent } from '../../elements/sidebar-nav/sidebar-nav.component';
 
-@Component({
-  selector: 'app-index',
-  imports: [HeaderAutenticadoComponent, SidebarNavComponent],
-  templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
-})
-export class IndexComponent {
+  @Component({
+    selector: 'app-index',
+    imports: [HeaderAutenticadoComponent, SidebarNavComponent],
+    templateUrl: './index.component.html',
+    styleUrl: './index.component.css',
+  })
+  export class IndexComponent {
+    @ViewChild('carrosselTendencias') carrosselTendencias!: ElementRef;
+    @ViewChild('carrosselStreetwear') carrosselStreetwear!: ElementRef;
 
-}
+    passarEsquerda(carrossel: string): void {
+      if (carrossel == 'tendencias') {
+        this.carrosselTendencias.nativeElement.scrollLeft -= 300;
+      } else if (carrossel == 'streetwear') {
+        this.carrosselStreetwear.nativeElement.scrollLeft -= 300;
+      }
+    }
+
+    passarDireita(carrossel: string): void {
+      if (carrossel == 'tendencias') {
+        this.carrosselTendencias.nativeElement.scrollLeft += 300;
+      } else if (carrossel == 'streetwear') {
+        this.carrosselStreetwear.nativeElement.scrollLeft += 300;
+      }
+    }
+  }
