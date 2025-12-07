304e52f6b6c425b71e6dca69d6b1839cb007a001
Luan <lsantana.duarte7@gmail.com>
2025-12-07 13:07:12 -0300

    feat, style(signup): criando verificação de senha e implementando local storage

--- src/app/components/pages/signup/signup.component.ts
+++ src/app/components/pages/signup/signup.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../elements/footer/footer.component';
 import { HeaderHomeComponent } from "../../elements/header-home/header-home.component";
+import { FormsModule } from '@angular/forms';
+import { LocalStorageService } from '../../../services/local-storage.service';
+import { v4 as uuid } from 'uuid';
+import { User } from '../../../models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [FooterComponent, HeaderHomeComponent],
+  imports: [FooterComponent, FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  nome = "";
+  sobrenome = "";
+  usuario = "";
+  email = "";
+  dataDeNascimento = "";
+  senha = "";
+  confirmacaoSenha = "";
+  termos: boolean = false;
+
+  constructor(private storage: LocalStorageService) {}
+
+  criarConta() {
+    const newUser: User = {
+      id: uuid(),
+      nome: this.nome,
+      sobrenome: this.sobrenome,
+      usuario: this.usuario,
+      email: this.email,
+      dataDeNascimento: this.dataDeNascimento,
+      senha: this.senha
+    }
+    
+    this.storage.salvarUsuario(newUser);
+  }
+}
