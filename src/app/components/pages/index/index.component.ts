6da6c1acfd90cd004c0c285dbac41ae7b7c49680
Luan <lsantana.duarte7@gmail.com>
2025-12-08 19:21:32 -0300

    feat(index): melhorando exibição de produtos

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -1,31 +1,92 @@
-  import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
-  import { HeaderAutenticadoComponent } from '../../elements/header-autenticado/header-autenticado.component';
-  import { SidebarNavComponent } from '../../elements/sidebar-nav/sidebar-nav.component';
-import { FooterComponent } from "../../elements/footer/footer.component";
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { HeaderAutenticadoComponent } from '../../elements/header-autenticado/header-autenticado.component';
+import { SidebarNavComponent } from '../../elements/sidebar-nav/sidebar-nav.component';
+import { FooterComponent } from '../../elements/footer/footer.component';
+import { CommonModule, NgFor } from '@angular/common';
 
-  @Component({
-    selector: 'app-index',
-    imports: [HeaderAutenticadoComponent, SidebarNavComponent, FooterComponent],
-    templateUrl: './index.component.html',
-    styleUrl: './index.component.css',
-  })
-  export class IndexComponent {
-    @ViewChild('carrosselTendencias') carrosselTendencias!: ElementRef;
-    @ViewChild('carrosselStreetwear') carrosselStreetwear!: ElementRef;
+@Component({
+  selector: 'app-index',
+  imports: [HeaderAutenticadoComponent, SidebarNavComponent, FooterComponent, CommonModule, NgFor],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css',
+})
+export class IndexComponent {
+  public produtosLista1 = [
+    {
+      id: 1,
+      nome: 'Camisa Oversized',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto1.jpg',
+    },
+    {
+      id: 2,
+      nome: 'Short streetwear',
+      valor: 'R$ 49,99',
+      img: 'assets/img/index-produto2.jpg',
+    },
+    {
+      id: 3,
+      nome: 'Camisa Oversized',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto3.jpg',
+    },
+    {
+      id: 4,
+      nome: 'Óculos',
+      valor: 'R$ 29,99',
+      img: 'assets/img/index-produto4.jpg',
+    },
+    {
+      id: 5,
+      nome: 'Camisa Polo',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto5.jpg',
+    },
+  ];
 
-    passarEsquerda(carrossel: string): void {
-      if (carrossel == 'tendencias') {
-        this.carrosselTendencias.nativeElement.scrollLeft -= 300;
-      } else if (carrossel == 'streetwear') {
-        this.carrosselStreetwear.nativeElement.scrollLeft -= 300;
-      }
-    }
+  public produtosLista2 = [
+    {
+      id: 1,
+      nome: 'Conjunto de Jóias',
+      valor: 'R$ 169,99',
+      img: 'assets/img/index-produto6.jpg',
+    },
+    {
+      id: 2,
+      nome: 'Suéter',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto7.jpg',
+    },
+    {
+      id: 3,
+      nome: 'Camisa Oversized',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto1.jpg',
+    },
+    {
+      id: 4,
+      nome: 'Camisa Polo',
+      valor: 'R$ 69,99',
+      img: 'assets/img/index-produto5.jpg',
+    },
+  ]
 
-    passarDireita(carrossel: string): void {
-      if (carrossel == 'tendencias') {
-        this.carrosselTendencias.nativeElement.scrollLeft += 300;
-      } else if (carrossel == 'streetwear') {
-        this.carrosselStreetwear.nativeElement.scrollLeft += 300;
-      }
+  @ViewChild('carrosselTendencias') carrosselTendencias!: ElementRef;
+  @ViewChild('carrosselStreetwear') carrosselStreetwear!: ElementRef;
+
+  passarEsquerda(carrossel: string): void {
+    if (carrossel == 'tendencias') {
+      this.carrosselTendencias.nativeElement.scrollLeft -= 300;
+    } else if (carrossel == 'streetwear') {
+      this.carrosselStreetwear.nativeElement.scrollLeft -= 300;
     }
   }
+
+  passarDireita(carrossel: string): void {
+    if (carrossel == 'tendencias') {
+      this.carrosselTendencias.nativeElement.scrollLeft += 300;
+    } else if (carrossel == 'streetwear') {
+      this.carrosselStreetwear.nativeElement.scrollLeft += 300;
+    }
+  }
+}
