16ab3a3f718de6fbec765693a7c67972fb0ffdf0
Luan <lsantana.duarte7@gmail.com>
2025-12-10 17:31:24 -0300

    feat(home): criando textos e organizando links

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../elements/footer/footer.component';
 import { HeaderHomeComponent } from "../../elements/header-home/header-home.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, HeaderHomeComponent],
+  imports: [FooterComponent, HeaderHomeComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
