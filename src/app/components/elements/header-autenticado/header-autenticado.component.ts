3c76323dfc23ba9c74ef32af2863e734fe305f64
Luan <lsantana.duarte7@gmail.com>
2025-12-10 17:03:16 -0300

    feat(header-aut): implementando input de busca

--- src/app/components/elements/header-autenticado/header-autenticado.component.ts
+++ src/app/components/elements/header-autenticado/header-autenticado.component.ts
@@ -1,21 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginService } from '../../../services/login.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header-autenticado',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header-autenticado.component.html',
   styleUrl: './header-autenticado.component.css',
 })
 export class HeaderAutenticadoComponent {
   public usuario: any = null;
-  constructor(private loginService: LoginService) {}
+  public mudancaIcone = true;
+  public pesquisa = ""
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
     this.usuario = this.loginService.lerUsuarioLogado()?.usuario;
   }
 
+  mudarIcone(): void {
+    this.mudancaIcone = !this.mudancaIcone;
+  }
+
+  onBlur(): void {
+    if (!this.mudancaIcone) {
+      this.mudancaIcone = true;
+    }
+  }
+
+  limparPesquisa(input: HTMLInputElement): void {
+    this.pesquisa = "";
+    input.focus();
+  }
+
   public mostrarConfigPerfil: boolean = false;
-  
+
 }
